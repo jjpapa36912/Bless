4d4f86c6ba9064832d35d454192bd4e2e1df6b3b
djk <joojaepapa@gmail.com>
2025-05-06 10:53:35 +0900

    xcode build success

---
+++ src/main/java/com/joyful/bless/BlessApplication.java
@@ -0,0 +1,13 @@
+package com.joyful.bless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlessApplication.class, args);
+	}
+
+}
